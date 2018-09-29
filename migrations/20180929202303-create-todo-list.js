9383a6bd406228ab2a8960d02fae829d6e0ff322
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2018-09-29 22:49:50 +0100

    Add todolist and migration

--- migrations/20180929202303-create-todo-list.js
+++ migrations/20180929202303-create-todo-list.js
@@ -2,20 +2,15 @@ const logger = require('./../services/logger');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('TodoTasks', {
+    return queryInterface.createTable('TodoLists', {
       id: {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.UUID
       },
-      task: {
+      name: {
         type: Sequelize.STRING,
-        allowNull: false,
-      },
-      completed: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: false,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
@@ -27,6 +22,6 @@ module.exports = {
       }
     }).catch(error => logger.error(error));
   },
-  down: queryInterface => queryInterface.dropTable('TodoTasks')
+  down: queryInterface => queryInterface.dropTable('TodoLists')
     .catch(error => logger.error(error))
 };
---
+++ models/todolist.js
@@ -0,0 +1,56 @@
+const { Sequelize, Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const TodoList = sequelize.define('TodoList', {
+    name: DataTypes.STRING
+  }, {});
+  TodoList.associate = function(models) {
+    // associations can be defined here
+  };
+  return TodoList;
+};
+
+/**
+ * TodoList model class
+ *
+ * @class TodoList
+ * @extends {Model}
+ */
+class TodoList extends Model {
+  static metaFields = {
+    id: {
+      type: Sequelize.UUID,
+      primaryKey: true
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  };
+
+  /**
+   * Initializes the todolist model
+   *
+   * @static
+   * @param {*} sequelize - the sequelize obbject
+   * @returns {object} the TodoList model
+   * @memberof TodoList
+   */
+  static init(sequelize) {
+    const model = super.init(TodoList.modelFields, { sequelize });
+    return model;
+  }
+
+  /**
+   * TodoList association
+   *
+   * @static
+   * @param {*} models - models in the application
+   * @memberof TodoList
+   */
+  static associate(models) {
+
+  }
+}
+
+module.exports = TodoList;
